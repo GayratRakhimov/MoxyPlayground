cca7bd90fff1a8b4a17ff4e62c4d2a38695447b2
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-05-30 17:46:20 +0500

    Changed annotationProcessor to kapt

--- app/src/main/java/com/gayratrakhimov/moxyplayground/MainActivity.kt
+++ app/src/main/java/com/gayratrakhimov/moxyplayground/MainActivity.kt
@@ -3,7 +3,6 @@ package com.gayratrakhimov.moxyplayground
 import android.os.Bundle
 import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
-import com.arellomobile.mvp.presenter.ProvidePresenter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : MvpAppCompatActivity(), MainView {
@@ -11,16 +10,9 @@ class MainActivity : MvpAppCompatActivity(), MainView {
     @InjectPresenter
     lateinit var presenter: MainPresenter
 
-    @ProvidePresenter
-    fun providePresenter() = MainPresenter()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onResume() {
-        super.onResume()
         presenter.getIp()
     }
 
